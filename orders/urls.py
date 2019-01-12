3aa93430aa6f86d05a621bab48a984b7873981cf
priyank <priyankkumar88@gmail.com>
2019-01-13 00:43:43 +0530

    orders will now lands on search page and added accordian on search

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.index,name="index"),
-    path("search",views.search, name="search"),
+    path("",views.search,name="index"),
+    #path("search",views.search, name="search"),
     path("do-order-status-update",views.order_status_update,name="do-order-status-update"),
 ]
