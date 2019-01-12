6e7d01ae1438fa6cf49ba0e1d34e6672c745bc8d
priyank <priyankkumar88@gmail.com>
2019-01-12 22:06:50 +0530

    order search made available

---
+++ orders/templatetags/order_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+@register.simple_tag(name="url_replace")
+def url_replace(request, field, value):
+    dict_=request.GET.copy()
+    print(dir(dict_))
+    dict_[field]=value
+    return dict_.urlencode()
+
+@register.filter(name="to_str")
+def to_str(value):
+    return str(value)
