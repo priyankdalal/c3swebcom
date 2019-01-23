09a14ce545188d30300eb8ee032c7ce494f079f4
priyank <priyankkumar88@gmail.com>
2019-01-23 22:58:10 +0530

    added add new admin user in board

--- adminmanager/form.py
+++ adminmanager/form.py
@@ -7,3 +7,9 @@ class EditForm(forms.Form):
     password_string=forms.CharField(label="Password",required=False,widget=forms.PasswordInput(render_value=True,attrs={'class':'form-control'}))
     role=forms.ChoiceField(label="Role",required=False,choices=(("admin","Admin"),("operator","Operator")),widget=forms.Select(attrs={'class':'form-control'}))
     password_changed=forms.CharField(label="pass_change",widget=forms.HiddenInput(attrs={'value':'0'}))
+
+class AddForm(forms.Form):
+    name=forms.CharField(label="Name",required=True,widget=forms.TextInput(attrs={'class':'form-control'}))
+    password=forms.CharField(label="OrigPassword",required=False,widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password_string=forms.CharField(label="Password",required=True,widget=forms.PasswordInput(render_value=True,attrs={'class':'form-control'}))
+    role=forms.ChoiceField(label="Role",required=True,choices=(("admin","Admin"),("operator","Operator")),widget=forms.Select(attrs={'class':'form-control'}))
